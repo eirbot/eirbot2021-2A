b77fafd682c0b9e0f075e5d3964eddc7d14459f9
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-03-05 16:05:54 +0100

    CODE : add documentation for workflow test

--- code/src/action.hpp
+++ code/src/action.hpp
@@ -2,14 +2,20 @@
 #define __ACTION_H_
 #include "tools.cpp"
 
+/**
+ * @fle  action.hpp*/
+
+/**
+ * @class Action
+ * @brief Permet de définir un actionneur*/
 class Action: public Debug{
     private :
-        const std::string _name;
-        int _state;
+        const std::string _name; /*!< Nom de l'actionneur */
+        int _state; /*!< État de l'actionneur (0 désactivé, 1 activé)*/
     public :
-        Action(const std::string);
+        Action(const std::string); /*!< Constructeur avec un nom (état à 0 par défaut)*/
         ~Action();
-        void changeState();
+        void changeState(); /*!< Réalise les actions pour changer l'état de l'actionneur */
         void printDebug();
 };
 
--- code/src/tools.cpp
+++ code/src/tools.cpp
@@ -1,17 +1,26 @@
 #include <string>
 #include <vector>
 
+/**
+ * @file tools.cpp*/
+
+/**
+ * @class Debug
+ * @brief Classe permettant d'afficher le debug de toutes les autres classes*/
 class Debug {
 private:
-    const bool _debug;
+    const bool _debug;/*!< Permet de définir si le debug est activé */
   public:
-    Debug(bool);
+    Debug(bool);/*!< Instancie la classe Debug, si bool vaut true alors le debug est activé*/
     ~Debug();
+
+  /**
+   * @brief Appel printDebug si debug vaut true*/
   void debugable() {
     if (_debug == true)
       this->printDebug();
   }
-  const virtual void printDebug() = 0;
+  const virtual void printDebug() = 0;/*!< Fonction virtuelle pure permettant d'obtenir une méthode printDebug dans toutes les classes filles */
 };
 
 class Position{
